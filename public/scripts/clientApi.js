a0990b63b502ad7780e453e253bac4a2b897b811
Viktoriya <step.vikulya@mail.ru>
2023-01-14 22:34:28 +0700

    Добавлен функционал записи в базу данных при победе

--- public/scripts/clientApi.js
+++ public/scripts/clientApi.js
@@ -1,35 +1,35 @@
 document.getElementById('form').addEventListener('submit', async (event) => {  
   event.preventDefault();
 
-  const userName = event.currentTarget[0].value;
+  const entryField = document.getElementById('input');
+  const userName = entryField.value;
+  const checkValidateName = userName => userName.match(/^[a-z0-9_-]{3,16}$/);
   
-  if (validateName(userName)) {
+  if (checkValidateName(userName)) {
     const response = await send(userName, timer.timeResult);
     const responseText = await response.text();
-    
-    if (response.status = 200) {
-      console.log("-Data sent on server");
-      document.querySelector('.form-wrapper').innerHTML = '<h2 class="form__title">Your score has been recorded!</h2>'
+
+    if (response.ok) {
+      console.log(`name:${userName} time:${timer.timeResult} id:${responseText} added on DB`);
+      document.querySelector('.form-wrapper').innerHTML = `<h2 class="form__title">Your record is recorded in the table</h2>`
       setTimeout(() => {
         document.querySelector('.form-container').classList.remove('form--open');
       }, 1500);
     }
   }
   else {
-    alert('Невалидно')
+    alert('Invalid value');
   }
 });
 
-function validateName(userName) {
-  var nameRegex = /^[a-z0-9_-]{3,16}$/;
-  return userName.match(nameRegex);
+
+async function send(username, time) {
+  return await fetch('/api/v1/record', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({username, time})
+  })
 }
 
-  async function send(name, time) {
-    return await fetch('/api/v1/record', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, time })
-    })
-  }
+
 
--- server.js
+++ server.js
@@ -1,50 +1,66 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const WebSocket = require('ws');
+const express = require("express");
+const bodyParser = require("body-parser");
+const WebSocket = require("ws");
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
-const PORT =  process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT);
-const socketServer = require('http').createServer(app).listen(server);
-const wss = new WebSocket.Server({ server:socketServer });
+const socketServer = require("http").createServer(app).listen(server);
+const wss = new WebSocket.Server({ server: socketServer });
+const url = "mongodb+srv://admin:12345@cluster0.ubn9cdr.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use(express.static(__dirname + '/public'))
-app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 const timer = {
   timeOnStart: undefined,
-  startTime()  {
+  startTime() {
     this.timeOnStart = Date.now();
-  }
-}
+  },
+};
 
-
-wss.on('connection', function connection(ws) {
-  console.log('--WebSocket on server connected...');
+wss.on("connection", function connection(ws) {
+  console.log("-WebSocket connected...");
   let tick;
 
-  ws.on('message', message => {
-
-    if (message == 'start') {
+  ws.on("message", (message) => {
+    if (message == "start") {
       timer.startTime();
       tick = setInterval(() => {
         ws.send(Date.now() - timer.timeOnStart);
       }, 0);
     }
 
-    if (message == 'end') {
+    if (message == "end") {
       clearInterval(tick);
-      console.log('--WebSocket on server closed...');
+      console.log("-WebSocket closed");
     }
   });
 });
 
 
-app.post('/api/v1/record', (response, request) => {
-  console.log(response.body);
-  request.send('okok');
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 
+app.post("/api/v1/record", async (res, req) => {
+  try {
+    await client.connect();
+    const result = await client
+      .db("results")
+      .collection("users")
+      .insertOne(res.body);
+    req.send(result.insertedId);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.close();
+  }
+});
